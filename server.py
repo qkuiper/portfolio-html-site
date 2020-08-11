2c79255a0388ad80e197c788f0f85e047429a395
Quido Kuiper <68793074+qkuiper@users.noreply.github.com>
2020-08-12 01:26:29 +0200

    functional website, apart from retreiving form data

--- server.py
+++ server.py
@@ -3,17 +3,13 @@ from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def my_home():
     return render_template('index.html')
 
-@app.route('/index.html')
-def index():
-    return render_template('index.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
-@app.route('/about.html')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact.html')
-def contact():
-    return render_template('contact.html')
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    return 'form submitted hoooray!'

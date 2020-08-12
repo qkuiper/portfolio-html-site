173c6ad900df45b90aead7d99474f66a508e785e
qkuiper <68793074+qkuiper@users.noreply.github.com>
2020-08-12 14:07:43 +0200

    Working form, need to add database

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
+import csv
+from flask import Flask, redirect, render_template, request, send_from_directory, url_for
 import os
-from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,6 +11,20 @@ def my_home():
 def html_page(page_name):
     return render_template(page_name)
 
+# def write_to_csv(data):
+#     with open('database.csv', mode='a') as database
+#         first_name = data['first_name']
+#         last_name = data['last_name']
+#         email = data['email']
+#         subject = data['subject']
+#         message = data['message']
+#         csv_writer = csv.writer(database, delimiter=',', quotecher='', quoting=csv.QUOTE_MINIMAL))
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
-    return 'form submitted hoooray!'
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        return redirect('thankyou.html')
+    else:
+        return 'something went wrong. Try again!'
